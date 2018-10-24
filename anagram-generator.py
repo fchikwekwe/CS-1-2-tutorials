6c0df1a97d0fe86f16960e7fbf3b00a95fe46208
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-23 20:01:44 -0700

    made the anagram converter check against dictionary of English words to make sure that output is viable

--- anagram-generator.py
+++ anagram-generator.py
@@ -1,24 +1,39 @@
 import sys
 import random
 
-words = open("words.txt", "r")
-print(words)
-letters = list(sys.argv[1])
+# import dictionary of most English words
+all_words = open("words.txt", "r")
+# convert into list of strings without new line
+word_list = all_words.read().replace("\n", " ").split()
+# take our command line parameter and convert into list
+original_word = list(sys.argv[1])
 
-def anagram_gen(letters):
+# v0 -- just reshuffles letter in word
+# def anagram_gen(letters):
+#     random.shuffle(letters)
+#     new_word = ''.join(letters)
+#     print(new_word)
+
+# v1 -- check if the result is an actual word against list of most English words
+def anagram_gen(search_word, all_words):
+    # using loop to make sure that we are rechecking if word does not qualify
     making_word = True
     while making_word:
-        random.shuffle(letters)
-        print(letters)
-        new_word = "".join(letters)
-        print(new_word)
-        if new_word not in words:
+        # shuffle it up
+        random.shuffle(search_word)
+        # form the list back into a string
+        new_word = "".join(search_word)
+        # check against imported dictionary
+        if new_word not in all_words:
             continue
+        # success condition
         else:
             break
-    return new_letters
-
+    # reform original word back into string
+    first_word = "".join(original_word)
+    # voila!
+    print("An anagram of '{}' is '{}'.".format(first_word, new_word))
 
 
 if __name__ in '__main__':
-    anagram_gen(letters)
+    anagram_gen(original_word, word_list)
