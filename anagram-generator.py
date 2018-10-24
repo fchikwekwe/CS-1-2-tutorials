420b5fe7935566bda3d700e84efa055a05ec12ae
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-23 18:03:06 -0700

    fixing bugs in anagram generator

--- anagram-generator.py
+++ anagram-generator.py
@@ -1,12 +1,24 @@
 import sys
 import random
 
+words = open("words.txt", "r")
+print(words)
 letters = list(sys.argv[1])
-# print(letters)
 
 def anagram_gen(letters):
-    random.shuffle(letters)
-    print(letters)
+    making_word = True
+    while making_word:
+        random.shuffle(letters)
+        print(letters)
+        new_word = "".join(letters)
+        print(new_word)
+        if new_word not in words:
+            continue
+        else:
+            break
+    return new_letters
+
+
 
 if __name__ in '__main__':
     anagram_gen(letters)
