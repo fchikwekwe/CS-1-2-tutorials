5e8695fffffbdcc1569d01881ecec2d9760c7b7d
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-23 20:36:57 -0700

    added comments to random incoherent sentence generator

--- dictionary_words.py
+++ dictionary_words.py
@@ -1,22 +1,29 @@
 import random
 import sys
 
+# importing built-in word list
 words = open("/usr/share/dict/words", "r")
+# making words into list
 word_list = words.read().replace("\n", " ").split()
-# print(word_list)
+# command line argument
 number_of_words = int(sys.argv[1])
-# print(number_of_words)
 
 def make_a_sentence(number_of_words):
+    # keep track of word count
     word_count = 0
+    # list to hold selected words
     sentence = []
+    # keep checking how many words we have
     while word_count < number_of_words:
+        # pick a random word from lists
         new_word = random.choice(word_list)
-        # print(new_word)
+        # add to list
         sentence.append(new_word)
+        # increment word count
         word_count += 1
-        # print(sentence)
+        # piece together our final words into a string
         final_sentence = ' '.join(sentence)
+    # voila!
     print("Your incoherent sentence of the day is:\n{}.".format(final_sentence.capitalize()))
 
 make_a_sentence(number_of_words)
