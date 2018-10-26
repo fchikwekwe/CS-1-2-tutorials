36086af353dfdd48fb271e5ef8c51bc82269a2fb
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-25 21:29:31 -0700

    added color module for clarity and ease of reading

--- vocab_game.py
+++ vocab_game.py
@@ -36,14 +36,15 @@ def choose_definition(vocab_list, random_index, definition_choice):
         random_word = random.choice(vocab_list)
         # get the index of that random word to compare laters
         random_index = vocab_list.index(random_word)
-        # print(random_index)
-        # print(definition_choice)
-        print(colored("Enter 'Q' at anytime to quit the game.", ))
+        print(colored("Enter 'Q' at anytime to quit the game.", "magenta"))
         definition_choice = input("Enter the number of the definition of {}: ".format(random_word))
 
         if definition_choice.lower() == 'q':
             print("Thanks for playing!")
             break
+        elif int(definition_choice) > len(definition_list) - 1:
+            print(colored("Please pick a number on the list!", "cyan"))
+            definition_choice = -1
         elif definition_choice.isdigit() != True:
             print("Please enter a number next time!")
             definition_choice = -1
@@ -51,10 +52,9 @@ def choose_definition(vocab_list, random_index, definition_choice):
             pass
 
         if random_index == int(definition_choice):
-            print("Great job! Let's try another one!")
+            print(colored("Great job! Let's try another one!", "green"))
         else:
-            print("That was not correct! The correct definition was:\n\n{} \n\nLet's try again with a new word.".format(definition_list[int(definition_choice)]))
-
+            print(colored("That was not correct! The correct definition was:\n\n{} \n\nLet's try again with a new word.", "red").format(definition_list[int(definition_choice)]))
 
 
 if __name__ in '__main__':
