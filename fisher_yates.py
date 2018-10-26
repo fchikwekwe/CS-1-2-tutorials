5aa905f2f327a58ceff49cfaa7b005add6175bf2
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-26 14:20:58 -0700

    making progress on fisher yates shuffle

--- fisher_yates.py
+++ fisher_yates.py
@@ -1,6 +1,20 @@
+import random
 
 def fisher_yates_shuffle(list):
+    list_length = len(list)
 
-    item_count
-    while len(list) > item_count:
-        
+    for item in range(len(list)):
+        random_item = random.choice(list)
+        first_item = item
+        print("first item {}".format(first_item))
+        item = random_item
+        print("item {}".format(item))
+        random_item = first_item
+        print("random item {}".format(random_item))
+        print(list)
+    return list
+
+
+if __name__ in '__main__':
+    list = ["0", "1", "2", "3", "4", "5"]
+    fisher_yates_shuffle(list)
