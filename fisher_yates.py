b70f7c242fbbce3f292e3a2a60ae09d6495b153f
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-26 14:45:48 -0700

    got my fisher yates shuffle working

--- fisher_yates.py
+++ fisher_yates.py
@@ -1,20 +1,18 @@
 import random
 
 def fisher_yates_shuffle(list):
-    list_length = len(list)
-
-    for item in range(len(list)):
+    # if there are items to shuffle
+    for item in list:
+        # pick a random element
         random_item = random.choice(list)
-        first_item = item
-        print("first item {}".format(first_item))
-        item = random_item
-        print("item {}".format(item))
-        random_item = first_item
-        print("random item {}".format(random_item))
-        print(list)
+        # get the indices of those elements
+        item_index = list.index(item)
+        random_item_index = list.index(random_item)
+        # swap it with the current element
+        list[item_index], list[random_item_index] = list[random_item_index], list[item_index]
     return list
 
 
 if __name__ in '__main__':
-    list = ["0", "1", "2", "3", "4", "5"]
-    fisher_yates_shuffle(list)
+    list = ["a", "b", "c", "d", "e", "f"]
+    print(fisher_yates_shuffle(list))
