dbbc9ed3436439f7aca510b594cd20448286a010
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-29 22:28:22 -0700

    started on list of counts

---
+++ counts_histogram_maker.py
@@ -0,0 +1,12 @@
+"""
+importing histogram_maker to use get_source_text and unique_words functions
+"""
+import histogram_maker
+
+def counts_histogram(source_text):
+    pass
+
+
+if __name__ in '__main__':
+    source_text = histogram_maker.get_source_text("souls_of_black_folk.txt")
+    # counts_histogram = counts_histogram(source_text)
