78894676ca930f06fdfaa84acd7eb6904651cb5d
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2018-10-30 14:04:35 -0700

    refactoring code; added three functions for modularity

--- counts_histogram_maker.py
+++ counts_histogram_maker.py
@@ -4,9 +4,37 @@ importing histogram_maker to use get_source_text and unique_words functions
 import histogram_maker
 
 def counts_histogram(source_text):
-    pass
+    # takes in source text and returns a histogram in the form of a list of lists
+    initial_histogram = [] # empty list to store histogram
+    # checks each word in the source text
+    for word in source_text:
+        # lists that will go inside of histogram list
+        count_and_word = []
+        # counts up the words in the source text and adds count and word to list
+        word_count = source_text.count(word)
+        count_and_word.append(word_count)
+        count_and_word.append(word)
+        # then adds the smaller list to the histogram list
+        initial_histogram.append(count_and_word)
+    # removes duplicates from initial_histogram and adds to new histogram list
+    print(initial_histogram)
+    no_duplicates = [] # empty list to store unduplicated lists
+    for list in initial_histogram:
+        if list not in no_duplicates:
+            no_duplicates.append(list)
+        else:
+            pass
+    histogram = []
+    print(no_duplicates)
+    # check the zero index of each list in no_duplicates
+    for list in no_duplicates:
+    # compare that zero index to the zero indices of all lists in histogram
+    # if there is no match, the append the entire list
+    # if there is a match, then add the first index of that list to the matching list
+
 
 
 if __name__ in '__main__':
     source_text = histogram_maker.get_source_text("souls_of_black_folk.txt")
-    # counts_histogram = counts_histogram(source_text)
+    counts_histogram = counts_histogram(source_text)
+    # print(counts_histogram)
--- dictionary_words.py
+++ dictionary_words.py
@@ -3,38 +3,45 @@ import sys
 import time # needed to record performance time
 import datetime # needed to record when trials are done
 
-# importing built-in word list
-words = open("/usr/share/dict/words", "r")
-# making words into list
-word_list = words.read().replace("\n", " ").split()
+def get_source_text(file):
+    # importing built-in word list
+    words = open(file, "r")
+    # making words into list
+    word_list = words.read().replace("\n", " ").split()
+    return word_list
 
-def make_a_sentence(number_of_words):
+def get_random_words(number_of_words, word_list):
     # keep track of word count
     word_count = 0
     # list to hold selected words
-    sentence = []
+    words_for_sentence = []
     # keep checking how many words we have
     while word_count < number_of_words:
         # pick a random word from lists
         new_word = random.choice(word_list)
         # add to list
-        sentence.append(new_word)
+        words_for_sentence.append(new_word)
         # increment word count
         word_count += 1
         # piece together our final words into a string
-    return sentence
+    return words_for_sentence
+
+def make_a_sentence():
+    pass
+
+def logger(file):
+    f = open(file, "a")
+    f.write("\n\nCurrent date and time: {} \nProgram ran in {} seconds.".format(datetime.datetime.now(), time.process_time() - start_time))
 
 if __name__ in '__main__':
     # command line argument
     number_of_words = int(sys.argv[1])
     # logging program performance time to dictionary word logger file
     start_time = time.process_time()
-
-    final_sentence = ' '.join(make_a_sentence(number_of_words))
+    word_list = get_source_text("/usr/share/dict/words")
+    final_sentence = ' '.join(get_random_words(number_of_words, word_list))
     # voila!
     print("Your incoherent sentence of the day is:\n{}.".format(final_sentence.capitalize()))
 
     # unsure how to write program to optimize more for speed; seems speedy to me
-    # disabled logging function until needed
-    f = open("dictionary_words_logger.txt", "a")
-    f.write("\n\nCurrent date and time: {} \nProgram ran in {} seconds.".format(datetime.datetime.now(), time.process_time() - start_time))
+    logger("dictionary_words_logger.txt")
